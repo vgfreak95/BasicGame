de2e11d845ba89fd36088ac0d6e86ddbf856a803
Henry <henry.fu897@gmail.com>
2024-06-10 18:55:34 -0400

    added pause screen functionality to the game

---
+++ scripts/pause.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+func _ready():
+	hide()
+
+
+func resume():
+	hide()
+	get_tree().paused = false
+	
+	
+func pause():
+	show()
+	get_tree().paused = true
+	
+	
+func escPause():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+
+
+func _on_resume_pressed():
+	resume()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	escPause()
